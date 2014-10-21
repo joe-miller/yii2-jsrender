b646ab1a556b84809003ac3e828daee7abc3ea49
sheillendra <sheillendra@yahoo.com>
2014-10-21 07:13:24 +0700

    change to bower

--- JsrenderAsset.php
+++ JsrenderAsset.php
@@ -6,8 +6,10 @@ use yii\web\AssetBundle;
 
 class JsRenderAsset extends AssetBundle
 {
-	public $sourcePath = '@sheillendra/jsrender';
+	public $sourcePath = '@bower/jsrender';
 	public $css = [];
 	public $js = ['jsrender.min.js'];
-	public $depends = [];
+	public $depends = [
+            
+        ];
 }
